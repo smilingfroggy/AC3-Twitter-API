38404140d5f8603650de19d2c69cbe9f18d42f5d
Martin-no1tw <kljhger7728@gmail.com>
2021-12-10 20:04:54 +0800

    feat: add publicmessage model

---
+++ models/publicmessages.js
@@ -0,0 +1,12 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const publicMessages = sequelize.define('publicMessages', {
+    id: DataTypes.INTEGER,
+    userId: DataTypes.INTEGER,
+    content: DataTypes.STRING
+  }, {});
+  publicMessages.associate = function(models) {
+    // associations can be defined here
+  };
+  return publicMessages;
+};
