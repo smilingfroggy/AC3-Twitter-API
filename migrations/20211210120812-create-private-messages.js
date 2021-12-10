824c551caf8c6318f786ae200e88832bcb234652
Martin-no1tw <kljhger7728@gmail.com>
2021-12-10 20:12:55 +0800

    feat:create privatemessages model

---
+++ migrations/20211210120812-create-private-messages.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('privateMessages', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      id: {
+        type: Sequelize.INTEGER
+      },
+      SenderId: {
+        type: Sequelize.INTEGER
+      },
+      RecieverId: {
+        type: Sequelize.INTEGER
+      },
+      RoomId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('privateMessages');
+  }
+};
---
+++ models/privatemessages.js
@@ -0,0 +1,13 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const privateMessages = sequelize.define('privateMessages', {
+    id: DataTypes.INTEGER,
+    SenderId: DataTypes.INTEGER,
+    RecieverId: DataTypes.INTEGER,
+    RoomId: DataTypes.INTEGER
+  }, {});
+  privateMessages.associate = function(models) {
+    // associations can be defined here
+  };
+  return privateMessages;
+};
