0cd0880ed43b6012cdf0eb6bfbe14b12b3b5e075
smilingfroggy <smilingfroggy@hotmail.com>
2021-12-12 01:22:51 +0800

    adjust socketController - private messages

--- controllers/socketController.js
+++ controllers/socketController.js
@@ -9,69 +9,85 @@ const socketController = {
     return User.findByPk(UserId, {
       attributes: ['id', 'name', 'avatar', 'account']
     }).then(user => {
-      PublicMessage.create({   //待確認是否儲存成功
-        UserId,
-        content: "上線",
-        type: "notice"
-      })
       user = user.toJSON()
-      user.content = "上線"
+      // user.content = "上線"   // 由socket.js處理
+      // user.type = "notice"    //已確認不寫入資料庫/歷史訊息不顯示notice(上下線訊息)
       // console.log(user)
       return user
     })
   },
-  savePublicMessages: (data) => { //輸入格式待確認：type要存?
+  savePublicMessages: (data) => {
     const { content, id } = data
     return Promise.all([
       PublicMessage.create({
-        content, UserId: id, // type
+        content, UserId: id, type: "message" // type
       }),
       User.findByPk(id, {
         attributes: ['id', 'name', 'avatar', 'account']
       })
     ])
-      .then(([newMessages, user]) => { return ([newMessages, user]) })
-  },
-  savePrivateMessages: (messages) => { //輸入格式待確認：傳入roomId? type要存?
-    // { name: "Majer", content: "吃早餐", id: 1, type: "message" }
-    const { content, UserId, type, RoomId } = messages
-    return PrivateMessage.create({
-      content, UserId, RoomId
-    })
+      .then(([newMessages, user]) => { 
+        user = user.toJSON()
+        return ([newMessages, user]) 
+      })
   },
   getPublicMessages: () => {
     return PublicMessage.findAll({
       raw: true,
       nest: true,
       include: { model: User, attributes: ['id', 'name', 'avatar', 'account'] },
-      attributes: ['content', 'createdAt'],
+      attributes: ['id', 'content', 'createdAt'],
       order: [['createdAt', 'ASC']]
     })
       .then(messages => {
         console.log(messages)
-        const sampleMessages = [
-          { content: "阿囉哈", createdAt: "2021-12-10 03:46:51", User: { id: 1, account: 'JENNY', name: 'JEN', avatar: '' } },
-          { content: "晚餐吃甚麼？", createdAt: "2021-12-11 03:46:51", User: { id: 2, account: 'JENNY', name: 'JEN', avatar: '' } }
-        ]
-        return sampleMessages
+        return messages
       })
   },
   getRoomId: (UserId, UserId2) => {   //建立新 private room
-    return Room.create({
+    return Room.findOrCreate({   //+ include user
       UserId, UserId2
     })
       .then(room => { return room.id })
   },
-  getPrivateMessages: async (roomId) => {
-    return await PrivateMessage.findAll({
-      where: { roomId },
+  savePrivateMessages: (data) => {
+    const { content, receiverId, senderId, RoomId } = data
+    return Promise.all([
+      PrivateMessage.create({
+        content, receiverId, senderId, RoomId
+      }),
+      User.findByPk(senderId, {
+        attributes: ['id', 'name', 'avatar', 'account']
+      })
+    ])
+      .then(([newMessages, user]) => {
+        user = user.toJSON()
+        const newMessages = { room: RoomId }  // 外面再包一個roomID
+        newMessages.room.newMessages = newMessages
+        newMessages.room.user = user
+        return (newMessages)
+      })
+  },
+  getPrivateMessages2: (RoomId) => {    // Room難以2次關連到 userId, 
+    return Room.findByPk(RoomId, {
+      include: { model: User, }
+    })
+  },
+  getPrivateMessages: (UserId) => {   // 跟使用者有關的所有歷史訊息(而不是roomID)
+    return PrivateMessage.findAll({
+      raw: true,
+      nest: true,
+      where: { $or: { 
+        senderId: UserId, 
+        receiverId: UserId
+      } },    // 不用RoomId找
       include: { model: User, attributes: ['id', 'name', 'avatar', 'account'] },
-      attributes: [
-        'content',
-        'createdAt'
-      ],
+      attributes: ['id', 'content', 'createdAt'],
       order: [['createdAt', 'ASC']]
     })
+      .then(messages => {     // 
+        
+      })
   },
 }
 
