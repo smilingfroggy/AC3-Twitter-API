ba87a364ccb5120cde93c0b00f23e5a12ea52b62
smilingfroggy <smilingfroggy@hotmail.com>
2021-12-17 22:03:33 +0800

    fix publicLeave output and add fetchSocket feature

--- socket/socket.js
+++ socket/socket.js
@@ -31,26 +31,58 @@ module.exports = (server) => {
       const user = await socketController.getUser(id)
       user.content = "上線"
       user.type = "notice"
-      
+
       if (!onlineUsers.map(user => user.id).includes(user.id)) {  // 檢查排除重複加入上線使用者清單
         onlineUsers.push(user)
         console.log("onlineUsers: ", onlineUsers)
         socket.to('PublicRoom').emit('publicLogin', user, onlineUsers)
-      } 
+      }
       socket.emit('publicLogin', user, onlineUsers) //OK {...}, [{..},{..}] 加to PublicRoom會收不到；放在if之外，自己在沒有disconnected情況下回到聊天室時，才會有onlineUsers列表
-      
+
+      // fetchSockets 功能測試
+      const sockets = await io.in("PublicRoom").fetchSockets();
+      socket.data.userId = user.id
+      socket.data.userName = user.name
+      console.log("socket.rooms: ", socket.rooms, "socket.data: ", socket.data, "socket.handshakes: ", socket.handshakes);  //{ userId: 21, userName: 'Camille Gibson DVM' } socket.handshakes:  undefined
     });
 
-
+    // 離線－V1 重新整理才會離線、登出不會離線
     socket.on("publicLeave", async (id) => {
+      console.log(`=== Public Leave ===`) //只有在使用者按推文model視窗之後，才會偵測到transport close(disconnect reason)
       const user = await socketController.getUser(id)
-      for (let i = 0; i < onlineUsers.length; i ++) {
+      for (let i = 0; i < onlineUsers.length; i++) {
         if (onlineUsers[i].id === id) {
           onlineUsers.splice(i, 1)
         }
       }
-      socket.broadcast.emit('publicLogout', user, onlineUsers)
+      user.content = "下線"
+      user.type = "notice"
+      const result = user
+      result.onlineUsers = onlineUsers    // 剩餘的線上使用者清單
+      console.log("public leave outputs", result)
+      socket.to('PublicRoom').emit('publicLogout', user, onlineUsers)  // 若to PublicRoom.broadcast- TypeError: Cannot read property 'emit' of undefined
     });
+
+    // 離線－V2 待測試；無user, id資訊，需處理(預先存入socket.data.name?)
+    socket.on("disconnect", async (reason) => {
+      console.log(`=== Disconnected ===`)
+      console.log(`Name- ${socket.data.userName} disconnected due to ${reason}`) //OK / undefined ; server namespace disconnect/ transport close
+      const sockets = await io.in("PublicRoom").fetchSockets();
+      console.log("socket.rooms: ", socket.rooms, "socket.data: ", socket.data,)
+      socket.disconnect();
+      socket.leave('PublicRoom')
+      //   for (const room of socket.rooms) {
+      //     console.log('socket.rooms', room)
+      //     if (room == "PublicRoom") {
+      //       socket.to("PublicRoom").emit("publicLogout", `socket.id: ${socket.id} to PublicRoom`);
+      //     } else {
+      //       console.error(`=== disconnected from room ${room}! ===`)
+      //       socket.to(room).emit("publicLogout", `socket.id: ${socket.id}`);
+      //     }
+      //   }
+      //   socket.broadcast.emit('publicLogout', "onlineUsers to provide")
+    });
+
 
     //data= userId1, userId2
     socket.on("privateEnter", async (data) => {
